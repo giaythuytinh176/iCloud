3b06b1a0992437e4a6a71b3d02d55b599c7a89fb
LE DUC TAM <giaythuytinh176@gmail.com>
2021-01-21 06:25:36 +0700

    upload

--- FormRegister/src/app/app.module.ts
+++ FormRegister/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LoginComponent} from './login/login.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RegisterFinalComponent} from './register-final/register-final.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterFinalComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FormRegister/src/app/login/login.component.ts
+++ FormRegister/src/app/login/login.component.ts
@@ -7,7 +7,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup | undefined;
+  loginForm!: FormGroup;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
